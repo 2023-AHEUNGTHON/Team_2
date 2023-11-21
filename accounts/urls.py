532324e1750478855635319816df8af549fe84f0
junhyung001 <junhyung@gmaail.com>
2023-11-21 11:17:04 +0900

    ADD: CRUD function

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('<int:pk>/',views.detail, name='detail'),
+    path('create/', views.create, name='create'),
+    path('delete/<int:pk>', views.delete, name='delete'),
+    path('update/<int:pk>', views.update, name='update'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from accounts.models import Article
 
 def index(request):
-    return render(request,'accounts/index.html')
+    articles = Article.objects.all()
+    return render(request, 'accounts/index.html', {'articles': articles})
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    return render(request, 'accounts/detail.html', {'article': article})
+
+def create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        article = Article(title=title, content=content)
+        article.save()
+        return redirect('accounts:detail', pk=article.pk)
+    else:
+        return render(request, 'accounts/create.html')
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        article.title = request.POST.get('title')
+        article.content = request.POST.get('content')
+        article.save()
+        return redirect('accounts:detail', pk=article.pk)
+    else:
+        return render(request, 'accounts/update.html', {'article': article})
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        article.delete()
+        return redirect('accounts:index')
+    else:
+        return redirect('accounts:detail', article.pk)
