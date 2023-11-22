d37e9f0277cd8048141aa4a30738308e23bd8eae
junhyung001 <junhyung@gmaail.com>
2023-11-22 22:50:46 +0900

    Add : Add password change

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 # import 
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import update_session_auth_hash
 # Import inside project
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 
@@ -49,3 +50,15 @@ def update(request):
         form = CustomUserChangeForm(instance=request.user)
     context = {'form' : form}
     return render(request, 'accounts/update.html', context)
+
+def change_password(request):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect('articles:index')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {'form' : form}
+    return render(request, 'accounts/change_password.html', context)
