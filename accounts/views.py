e26f61efcec4ff1a05f452346d2a231bc6030901
junhyung001 <junhyung@gmaail.com>
2023-11-22 22:13:16 +0900

    Add : Membership registration function added

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
+# import 
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+# Import inside project
+from .forms import CustomUserCreationForm
 
 def login(request):
     if request.method == 'POST':
@@ -18,3 +21,14 @@ def login(request):
 def logout(request):
     auth_logout(request)
     return redirect('articles:index')
+
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {'form':form}
+    return render(request, 'accounts/signup.html', context)
