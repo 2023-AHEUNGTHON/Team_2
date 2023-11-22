86a8f2a380699d3a3e35b62993fa1a6510c6e03c
junhyung001 <junhyung@gmaail.com>
2023-11-22 21:41:27 +0900

    Add : Login function implementation completed

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,5 @@ from accounts import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
 
-def index(request):
-    return render(request, 'accounts.html')
+
+def login(request):
+    if request.method == 'POST':
+        # 로그인 처리를 해준다.
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('articles:index')
+    else:
+        form = AuthenticationForm()
+    context = {'form' : form}
+    return render(request, 'accounts/login.html', context)
