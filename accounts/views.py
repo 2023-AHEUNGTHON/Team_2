8ce43ca7b4a2104da972974145eb48766f0b17ce
junhyung001 <junhyung@gmaail.com>
2023-11-22 16:58:36 +0900

    Add : Added image upload function

--- accounts/views.py
+++ accounts/views.py
@@ -13,7 +13,7 @@ def detail(request, pk):
 
 def create(request):
     if request.method == 'POST':
-        form = ArticleForm(request.POST)
+        form = ArticleForm(request.POST, request.FILES)
         if form.is_valid():
             article = form.save()
             return redirect('accounts:detail', article.pk)
@@ -28,7 +28,7 @@ def update(request, pk):
     article = Article.objects.get(pk=pk)
 
     if request.method == "POST":
-        form = ArticleForm(request.POST, instance=article)
+        form = ArticleForm(request.POST, request.FILES,instance=article)
         if form.is_valid():
             form.save()
             return redirect('accounts:detail', pk=article.pk)
--- retry/settings.py
+++ retry/settings.py
@@ -125,3 +125,7 @@ STATICFILES_DIRS =[
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media file
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
