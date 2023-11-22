e36b7a4fb02fc7e5374edc7a166897b5543f31eb
junhyung001 <junhyung@gmaail.com>
2023-11-22 23:05:12 +0900

    Fix : Edit delete function

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import update_session_auth_hash
+from django.views.decorators.http import require_http_methods
 # Import inside project
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 
@@ -34,10 +35,11 @@ def signup(request):
     context = {'form':form}
     return render(request, 'accounts/signup.html', context)
 
-def delete(requeest):
-    user = requeest.user
-    user.delete()
-    auth_logout(requeest)
+
+@require_http_methods(['POST'])
+def delete(request):
+    request.user.delete()
+    auth_logout(request)
     return redirect('articles:index')
 
 def update(request):
