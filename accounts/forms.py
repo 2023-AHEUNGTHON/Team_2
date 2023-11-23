5cb38ba48ee7aa6146e155b92715bc7485b07a85
junhyung001 <junhyung@gmaail.com>
2023-11-24 01:22:24 +0900

    Fix : login email  field added

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
+from django import forms
 
 class CustomUserCreationForm(UserCreationForm):
-
+    email = forms.EmailField()
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
 
