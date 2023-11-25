ac6af0e25a80db43906609551fcd15a8b15d0d1c
junhyung001 <junhyung@gmaail.com>
2023-11-25 14:20:05 +0900

    Fix : form.py class 수정

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,29 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
 from django.contrib.auth import get_user_model
 from django import forms
+from django.forms import TextInput, EmailInput, NumberInput  # 추가
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField()
+
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
+        fields = ('email', 'username', 'password')
+        widgets = {
+            'email': EmailInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Email'
+            }),
+        }
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'loginform'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'loginform'}))
+
+    class Meta:
+        fields = ('username', 'password')
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = get_user_model()
-        fields = ('email', 'first_name', 'last_name')
--- articles/forms.py
+++ articles/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from .models import Article, Comment
 
 class ArticleForm(forms.ModelForm):
-    
     class Meta:
         model = Article
         fields = '__all__'
